b488793890dddedc5864fea78763d53a391078b9
Tanishka <115578855+tanishkasonar22@users.noreply.github.com>
2025-07-05 01:20:51 +0530

    Create Problem 2.py
    I am confused what to do next

---
+++ Problem 2.py
@@ -0,0 +1,20 @@
+def minmax_arr(nums):
+  n= len(nums)
+  i = 0
+
+  if nums == 0;
+    return None
+
+  while i < n-1:
+    if nums[i] < nums [i+1]
+      min = nums[i]
+      max = nums[i+1]
+    else:
+      min = nums[i+1]
+      max = nums[i]
+  return min,max
+    
+    
+                 
+
+                 
